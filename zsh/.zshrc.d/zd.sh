6ee5e6bdbec454287c0386953398a56c10013773
deorbil <80085032+deorbil@users.noreply.github.com>
2025-04-21 17:13:22 +0700

    feat(zsh): add zd

---
+++ zsh/.zshrc.d/zd.sh
@@ -0,0 +1,19 @@
+export ZD_DIR="$HOME/.zd"
+
+# Override the default picker
+# ZD_PICKER="fzf --print-query"
+
+# Override the picker args
+# ZD_PICKER_ARGS=""
+
+# Define which plugins to enable
+ZD_PLUGINS=(cd tmux)
+
+# Uncomment the following line to disable default aliases
+# ZD_PLUGINS_DISABLE_ALIAS="true"
+
+# Override the default rc file location
+# This is where you will define your directories
+# ZD_RC="$HOME/.zdrc.sh"
+
+source "$ZD_DIR/zd.sh"
