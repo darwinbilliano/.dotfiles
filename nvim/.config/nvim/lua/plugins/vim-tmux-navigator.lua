f54b43fe864d11ec01757ecb4fc12ae6a781e1fd
deorbil <80085032+deorbil@users.noreply.github.com>
2025-05-21 07:39:06 +0700

    fix(nvim): vim tmux navigator lazy trigger

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,5 +1,13 @@
 return {
 	"christoomey/vim-tmux-navigator",
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+		"TmuxNavigatorProcessList",
+	},
 	keys = {
 		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
 		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
